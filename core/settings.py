33aab176591d54648edfecc95ff458a667d43c6f
David Gaytan <edgaytanc@gmail.com>
2024-04-02 01:07:02 -0600

    se agrego el login

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Opcional: define un lugar para recopilar archivos estáticos con `collectstatic`
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Especifica los directorios donde Django buscará archivos estáticos adicionales, aparte de cada app 'static' folder.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -126,4 +135,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home'
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+    # def get_success_url(self):
+    #     return reverse_lazy('home')  # O la URL que prefieras
+
+@login_required
+def home(request):
+    # Asegúrate de que 'get_full_name()' es un método válido de tu 'CustomUser'
+    return render(request, 'index.html', {'nombre_usuario': request.user.username})
+
+
